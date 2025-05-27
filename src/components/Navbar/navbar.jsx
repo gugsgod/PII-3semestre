20e13577fea7bbf45ebac14ebe7c438f7be3591e
Nicole <nsmascaretti@gmail.com>
2025-05-27 16:33:00 -0300

    tirando o avatar P

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,13 +1,11 @@
 import './navbar.css';
 import logo from '../../assets/logo_poliedro.png';
-import avatar from '../../assets/avatar_professor.png';
 
 function Navbar(props) {
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
       <div className="user-info">
-        <img src={avatar} alt="Avatar do usuário" className="avatar-image" />
         <span>{props.usuario}</span>
       </div>
     </nav>
