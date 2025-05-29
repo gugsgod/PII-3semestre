705e9982e818fdb524ae3d211c0863ee8e6cafa4
Caio Onha Ferreira <caioonha@gmail.com>
2025-05-29 09:00:42 -0300

    background fantasma

--- src/components/Tabela/Tabela.jsx
+++ src/components/Tabela/Tabela.jsx
@@ -1,37 +1,46 @@
-import icon from "../../assets/excluir.png"
+import icon from "../../assets/excluir.png";
 
 const Tabela = (props) => {
-    return (
+  return (
+    // Fundo
+    <div className="w-full max-w-xl bg-white rounded-3xl ">
+      {/* Titulo */}
+      <div className="bg-gray-200 rounded-t-3xl px-6 py-4">
+        <h1 className="text-3xl text-gray-700 text-center">{props.titulo}</h1>
+      </div>
 
-        // Fundo
-        <div className="w-full max-w-xl bg-white rounded-3xl ">
+      <div className="px-6 py-6 space-y-4 max-h-64 overflow-auto">
+        <table className="min-w-full table-auto">
+          <tbody>
+            <tr className="hover:bg-gray-50">
+              <td className="px-4 py-2 border-b">{props.atividade1}</td>
+              <td className="px-4 py-2 border-b">
+                <button className="rounded-3xl hover:bg-red-500 bg-white">
+                  <img src={icon} alt="" className="w-10" />
+                </button>
+              </td>
+            </tr>
+            <tr className="hover:bg-gray-50">
+              <td className="px-4 py-2 border-b">{props.atividade2}</td>
+              <td className="px-4 py-2 border-b">
+                <button className="rounded-3xl bg-white hover:bg-red-500">
+                  <img src={icon} alt="" className="w-10" />
+                </button>
+              </td>
+            </tr>
+            <tr className="hover:bg-gray-50">
+              <td className="px-4 py-2 border-b">{props.atividade3}</td>
+              <td className="px-4 py-2 border-b">
+                <button className="rounded-3xl bg-white hover:bg-red-500">
+                  <img src={icon} alt="" className="w-10" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-            {/* Titulo */}
-            <div className="bg-gray-200 rounded-t-3xl px-6 py-4">
-                <h1 className="text-3xl text-gray-700 text-center">{props.titulo}</h1>
-            </div>
-
-            <div className="px-6 py-6 space-y-4 max-h-64 overflow-auto">
-                <table className="min-w-full table-auto">
-                    
-                    <tbody>
-                        <tr className="hover:bg-gray-50">
-                            <td className="px-4 py-2 border-b">{props.atividade}</td>
-                            <td className="px-4 py-2 border-b"><button className="rounded-3xl hover:bg-red-500 bg-white"><img src={icon} alt="" className="w-10"/></button></td>
-                        </tr>
-                        <tr className="hover:bg-gray-50">
-                            <td className="px-4 py-2 border-b">{props.atividade}</td>
-                            <td className="px-4 py-2 border-b"><button className="rounded-3xl bg-white hover:bg-red-500"><img src={icon} alt="" className="w-10"/></button></td>
-                        </tr>
-                        <tr className="hover:bg-gray-50">
-                            <td className="px-4 py-2 border-b">{props.atividade}</td>
-                            <td className="px-4 py-2 border-b"><button className="rounded-3xl bg-white hover:bg-red-500"><img src={icon} alt="" className="w-10"/></button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
-}
-
-export default Tabela
+export default Tabela;
--- src/screens/HomeCoordenacao/GerenciarPessoas/GerenciarPessoas.jsx
+++ src/screens/HomeCoordenacao/GerenciarPessoas/GerenciarPessoas.jsx
@@ -3,43 +3,52 @@ import { useNavigate } from "react-router-dom";
 
 import Tabela from "../../../components/Tabela/Tabela";
 
-import "./GerenciarPessoas.css"
+import "./GerenciarPessoas.css";
 
 import Titulo from "../../../components/Navbar/Titulo";
 
 const GerenciarPessoas = () => {
+  const navigate = useNavigate();
+  const tipo = localStorage.getItem("tipoUsuario");
 
-    const navigate = useNavigate()
-    const tipo = localStorage.getItem("tipoUsuario")
+  useEffect(() => {
+    if (tipo !== "coordenacao") {
+      navigate("/");
+    }
+  }, [tipo, navigate]);
 
-    useEffect(() => {
-        if (tipo !== "coordenacao") {
-            navigate("/")
-        }
-    }, [tipo, navigate])
-
-    useEffect(() => {
-    document.body.style.overflow = 'hidden';
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     };
   }, []);
 
-    return (
-        <div className="overflow-hidden">
-            <Titulo titulo="Pessoas cadastradas"/>
-            <div className="gerenciar-page">
-                <div className="flex justify-center gap-10">
-                    <div className="overflow-x-auto">
-                        <Tabela titulo="Alunos Cadastrados"/>
-                    </div>
-                    <div className="overflow-x-auto">
-                        <Tabela titulo="Professores Cadastrados"/>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="overflow-hidden">
+      <Titulo titulo="Pessoas cadastradas" />
+      <div className="gerenciar-page">
+        <div className="flex justify-center gap-10">
+          <div className="overflow-x-auto">
+            <Tabela
+              titulo="Alunos Cadastrados"
+              atividade1="Caio Onha Ferreira"
+              atividade2="Nicole Silva Mascaretti"
+              atividade3="Gustavo Noronha Bomfim"
+            />
+          </div>
+          <div className="overflow-x-auto">
+            <Tabela
+              titulo="Professores Cadastrados"
+              atividade1="Caio Onha Ferreira"
+              atividade2="Nicole Silva Mascaretti"
+              atividade3="Gustavo Noronha Bomfim"
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default GerenciarPessoas
+export default GerenciarPessoas;
