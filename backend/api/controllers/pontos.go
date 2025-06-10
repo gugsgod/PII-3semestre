fc3bcfadba4955091f7352f51f52171514c8249b
Gustavo Bomfim <mifmobng@gmail.com>
2025-06-10 14:42:06 -0300

    metade

--- backend/api/controllers/pontos.go
+++ backend/api/controllers/pontos.go
@@ -1,3 +1,11 @@
 package controllers
 
+import(
+	"net/http"
+	"backend/api/models"
+	"github.com/gin-gonic/gin"
+)
 
+func InserirPontos(c*gin.Context){
+	var pontos models.Pontos
+}
---
+++ backend/api/db/get_id.go
@@ -0,0 +1,7 @@
+package database
+
+import(
+	"database/sql"
+	"time"
+	_ "github.com/go-sql-driver/mysql"
+)
