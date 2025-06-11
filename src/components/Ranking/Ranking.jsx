72edb42908a02526d66585d1fbae7759dc9acba9
Nicole <nsmascaretti@gmail.com>
2025-06-11 15:32:04 -0300

    arrumando ranking da tela do professor

--- src/components/Ranking/Ranking.jsx
+++ src/components/Ranking/Ranking.jsx
@@ -1,55 +1,29 @@
+import "./RankingComponente.css"
+
 const rankingData = [
-  { pos: '1º', nome: 'Nicole Silva Mascaretti', pontos: 1500 },
-  { pos: '2º', nome: 'Gustavo Versolatto', pontos: 360 },
-  { pos: '3º', nome: 'Gustavo Noronha Bomfim', pontos: 15 },
+  { pos: "1º", nome: "Nicole Silva Mascaretti", pontos: 1500 },
+  { pos: "2º", nome: "Gustavo Versolatto", pontos: 360 },
+  { pos: "3º", nome: "Gustavo Noronha Bomfim", pontos: 15 },
 ];
 
-const Ranking = () => {
+const TabelaRanking = () => {
   return (
-      <div className="flex flex-col items-center justify-center px-4">
-        {/* Fundo */}
-      <div className="flex flex-col justify-center bg-gray-300 shadow-lg rounded-2xl w-full max-w-2xl min-h-[400px]">
-        {/* Titulo */}
-        <div className="bg-gray-400 text-center text-xl font-semibold py-4 rounded-t-2xl">
-          Ranking de desempenho
+    <div className="ranking-box1">
+      <div className="ranking-header1">Ranking de desempenho</div>
+      {rankingData.map((item, index) => (
+        <div className="ranking-content1" key={index}>
+          <div className="ranking-item">{item.pos}</div>
+          <div className="ranking-item">{item.nome}</div>
+          <div className="ranking-item">{item.pontos}</div>
         </div>
-        {/* Tabela */}
-        <table className="w-full text-center table-fixed bg-gray-300">
-          <thead>
-            <tr className="">
-              <th className="w-1/6 py-3 text-gray-600 border-r border-gray-400">Posição</th>
-              <th className="w-2/6 py-3 text-gray-600 border-r border-gray-400">Nome</th>
-              <th className="w-1/6 py-3 text-gray-600">Pontos</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rankingData.map((item, index) => (
-              <tr key={index} className="border-t border-b border-gray-400">
-                <td className="py-4 font-medium border-r border-r-gray-400 text-gray-800">{item.pos}</td>
-                <td className="py-4 border-r border-r-gray-400 text-gray-800">{item.nome}</td>
-                <td className="py-4 border-l border-gray-400 text-gray-800">{item.pontos}</td>
-              </tr>
-            ))}
-            {[...Array(5 - rankingData.length)].map((_, idx) => (
-              <tr key={`empty-${idx}`} className="h-14 border-b border-gray-400">
-                <td className="border-r border-gray-400">
-                </td>
-                <td className="border-r border-gray-400">
-                </td>
-                <td className="border-l border-gray-400">
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="flex justify-between items-center h-2 rounded-b-2xl overflow-hidden">
-          <div className="bg-red-500 h-full w-full sm:w-3/12"></div>
-          <div className="bg-blue-400 h-full  w-full sm:w-6/12"></div>
-          <div className="bg-yellow-400 h-full w-full sm:w-3/12"></div>
-        </div>
+      ))}
+      <div className="ranking-bar1">
+        <div className="bar red"></div>
+        <div className="bar blue"></div>
+        <div className="bar yellow"></div>
       </div>
     </div>
   );
 };
 
-export default Ranking;
+export default TabelaRanking;
--- src/screens/HomeProfessor/Ranking/Ranking.jsx
+++ src/screens/HomeProfessor/Ranking/Ranking.jsx
@@ -1,42 +1,33 @@
 import { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import "./Ranking.css"
-import TabelaRanking from "../../../components/Ranking/Ranking"
+import "./Ranking.css";
+import TabelaRanking from "../../../components/Ranking/Ranking";
 
 const Ranking = () => {
-    const navigate = useNavigate()
-    const tipo = localStorage.getItem("tipoUsuario")
+  const navigate = useNavigate();
+  const tipo = localStorage.getItem("tipoUsuario");
 
-    useEffect(() => {
-        if (tipo !== "professor") {
-            navigate("/")
-        }
-    }, [tipo, navigate])
+  useEffect(() => {
+    if (tipo !== "professor") {
+      navigate("/");
+    }
+  }, [tipo, navigate]);
 
-    useEffect(() => {
-    document.body.style.overflow = 'hidden';
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     };
   }, []);
 
-    return (
-        <div className="">
-            <div className="flex justify-end bg-transparent p-6">
-                <button className="bg-blue-900 text-white px-4 py-2 rounded-full text-sm hover:bg-blue-700">
-                    Tornar público
-                </button>
-            </div>
-            <div className="ranking-page">
-                <div className="flex justify-center">
-                    <div>
-                        <TabelaRanking/>       
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="aluno-page">
+      <div className="ranking-container">
+        <TabelaRanking />
+      </div>
+    </div>
+  );
+};
 
-export default Ranking
+export default Ranking;
