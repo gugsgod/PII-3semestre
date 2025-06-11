09e823ff3ff37c46c49fec620e2c15d4865348d7
Caio Onha Ferreira <caioonha@gmail.com>
2025-06-10 22:59:55 -0300

    update tela criar atividades

--- src/screens/HomeProfessor/CategoriasPontuacao/CriarAtividades.jsx
+++ src/screens/HomeProfessor/CategoriasPontuacao/CriarAtividades.jsx
@@ -32,11 +32,9 @@ const CriarAtividades = () => {
             <form action="">
             <label htmlFor="">Nome da atividade</label>
             <input type="text" name="" id="" />
-            <label htmlFor="">Pontos da atividade</label>
-            <input type="text" />
             </form>
         </div>
-        <div className="max-w-[500px] mx-auto mt-10">
+        <div className="max-w-[500px] mx-auto mt-20">
             <label htmlFor="">Atribuir atividade às turmas</label>
         </div>
         <div className="max-w-[500px] mx-auto mt-10 bg-white p-3 shadow-2xl max-h-[30vh] overflow-y-scroll">
