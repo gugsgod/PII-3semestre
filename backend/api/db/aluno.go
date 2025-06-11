a1424ce3e3ac80a7bf037c7672e4603d01620e25
Gustavo Bomfim <mifmobng@gmail.com>
2025-06-11 19:57:07 -0300

    back

---
+++ backend/api/db/aluno.go
@@ -0,0 +1,36 @@
+package database
+
+import(
+	"database/sql"
+	"backend/api/models"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Alunos(db *sql.DB) ([]models.NomeAluno, error){
+	var sliceAlunos []models.NomeAluno
+
+	query := `
+		SELECT id_aluno, nome
+		FROM users
+	`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next(){
+		var resultado models.NomeAluno
+		if err := rows.Scan(&resultado.ID, &resultado.NOME); err != nil {
+			return nil, err
+		}
+		sliceAlunos = append(sliceAlunos, resultado)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return sliceAlunos, nil
+}
