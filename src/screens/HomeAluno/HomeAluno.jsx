0f079547aebe09a5a3b875dc1a569dae6e7d82e1
Nicole <nsmascaretti@gmail.com>
2025-06-11 17:24:56 -0300

    botao sair tela home aluno

--- src/screens/HomeAluno/HomeAluno.jsx
+++ src/screens/HomeAluno/HomeAluno.jsx
@@ -21,21 +21,29 @@ function HomeAluno() {
     }, [tipo, navigate]);
 
     useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "auto";
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.clear();
+        navigate("/");
     };
-  }, []);
 
 
     return (
         <div>
-            <Navbar usuario="Olá, Aluno"/>
+            <Navbar usuario="Olá, Aluno" />
+            <div className="ms-4 mt-4 flex justify-start text-center p-1">
+                <button className="bg-[#D9D9D9] rounded-2xl p-1 w-24 shadow-lg hover:text-white" onClick={handleLogout}>Sair</button>
+            </div>
             <div className='aluno-page'>
                 <div className='card-grid2'>
-                    <Card icon={icon1} label="Meu pontos e atividades" size="medium"  onClick={() => navigate("/MeusPontosEAtividades")}/>
-                    <Card icon={icon2} label="Ranking" size="medium" className="card-ranking" onClick={() => navigate("/RankingAluno")}/>
-                  
+                    <Card icon={icon1} label="Meu pontos e atividades" size="medium" onClick={() => navigate("/MeusPontosEAtividades")} />
+                    <Card icon={icon2} label="Ranking" size="medium" className="card-ranking" onClick={() => navigate("/RankingAluno")} />
+
                 </div>
             </div>
         </div>
