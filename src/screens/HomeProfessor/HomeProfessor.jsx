913f3015d8ab3b3a7d21d04e9c6cb23c9721545a
Caio Onha Ferreira <caioonha@gmail.com>
2025-06-11 15:41:15 -0300

    arrumando

--- src/screens/HomeProfessor/HomeProfessor.jsx
+++ src/screens/HomeProfessor/HomeProfessor.jsx
@@ -37,14 +37,14 @@ function HomeProfessor() {
     <div>
       <Navbar />
       <div className="ms-4 mt-4 flex justify-start text-center p-1">
-        <button className="bg-[#D9D9D9] rounded-2xl p-1 w-24 shadow-lg" onClick>Sair</button>
+        <button className="bg-[#D9D9D9] rounded-2xl p-1 w-24 shadow-lg" onClick={handleLogout}>Sair</button>
       </div>
       <div className="professor-background">
         <div className="flex gap-4 ">
-          <Card icon={icon1} label={"Atribuir Pontos"}/>
-          <Card icon={icon2} label={"Ranking"}/>
-          <Card icon={icon3} label={"Categorias de Pontuação"}/>
-          <Card icon={icon4} label={"Atribuir Turmas"}/>
+          <Card icon={icon1} label={"Atribuir Pontos"} onClick={() => navigate("/TelainicialAtribuirPontos")}/>
+          <Card icon={icon2} label={"Ranking"} onClick={() => navigate("/Ranking")}/>
+          <Card icon={icon3} label={"Categorias de Pontuação"} onClick={() => navigate("/CategoriasPontuacao")}/>
+          <Card icon={icon4} label={"Atribuir Turmas"} onClick={() => navigate("/GerenciarTurmas")}/>
         </div>
       </div>
     </div>
