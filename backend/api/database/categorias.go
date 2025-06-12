cbc2abfc8812c5a5f51542c8b540618732772060
GVersolatto <gustavo.versolatto@gmail.com>
2025-06-12 05:10:11 -0300

    funcoes database para POST categorias

--- backend/api/database/categorias.go
+++ backend/api/database/categorias.go
@@ -3,7 +3,10 @@ package database
 import (
 	"backend/models"
 	"database/sql"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"strconv"
 )
 
 func Categorias(db *sql.DB) ([]models.Categorias, error) {
@@ -32,4 +35,40 @@ func Categorias(db *sql.DB) ([]models.Categorias, error) {
 		return nil, err
 	}
 	return sliceCategorias, nil
+}
+
+func adicionarCategoria(db *sql.DB, c *gin.Context) {
+	type Categoria struct {
+		ID     int64  `json:"id_pontuacao"`
+		Nome   string `json:"nome_pontuacao"`
+		Pontos int64  `json:"pontos"`
+	}
+
+	var cat Categoria
+	if err := c.BindJSON(&cat); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err := db.Exec("INSERT INTO categoria (id_pontuacao, nome_pontuacao, pontos) VALUES (?, ?, ?)", cat.ID, cat.Nome, cat.Pontos)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "categoria adicionada"})
+}
+
+func removerCategoria(db *sql.DB, c *gin.Context) {
+	param := c.Param("id")
+	id, err := strconv.Atoi(param)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID inválido"})
+		return
+	}
+	_, err = db.Exec("DELETE FROM categoria WHERE id_pontuacao = ?", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "categoria removida"})
 }
