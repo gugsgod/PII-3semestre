84f25943cf7147ba5db8b9c276e14e7d736361be
GVersolatto <gustavo.versolatto@gmail.com>
2025-06-12 01:46:36 -0300

    Sinistro

---
+++ backend/api/database/categorias.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"backend/models"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Categorias(db *sql.DB) ([]models.Categorias, error) {
+	var sliceCategorias []models.Categorias
+
+	query := `
+		SELECT nome, pontos
+		FROM categorias
+	`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var resultado models.Categorias
+		if err := rows.Scan(&resultado.NOME, &resultado.PONTOS); err != nil {
+			return nil, err
+		}
+		sliceCategorias = append(sliceCategorias, resultado)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return sliceCategorias, nil
+}
+
